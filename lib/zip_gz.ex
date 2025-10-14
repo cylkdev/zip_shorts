daad26146be310ad43ac1dd00a2818bf2d879bb1
Kurt Hogarth <87607684+cylkdev@users.noreply.github.com>
2025-10-14 11:16:24 -0400

    improve buffer performance

--- lib/zip_gz.ex
+++ lib/zip_gz.ex
@@ -1,33 +1,35 @@
 defmodule ZipGz do
   @moduledoc """
-  `ZipGz` provides an API for working with ZIP and GZIP streams, enabling efficient
-  handling of large files.
+  `ZipGz` provides tools for creating ZIP and GZIP streams, enabling efficient handling
+  of large files through streaming.
 
-  `ZipGz` is designed to work with streams, allowing you to compress data on-the-fly.
-  It supports both ZIP and GZIP formats, with options for customizing compression
-  levels and chunk sizes.
+  This module is designed to work with streams, allowing you to compress data on-the-fly.
+  It supports both ZIP and GZIP formats, with options for customizing compression levels
+  and chunk sizes.
 
   ## Examples
 
   ### Creating a ZIP stream with a custom chunk size
 
-      iex> ZipGz.stream([%{path: "rel/path/file.txt", source: ["content"]}], chunk_size: 10)
+      iex> entries = [%{path: "rel/path/file.txt", source: ["content"]}]
+      ...> ZipGz.stream(entries, chunk_size: 10)
 
   ### Creating a ZIP stream with GZIP compression
 
-      iex> text = Enum.map_join(1..1_024, "", fn _ -> String.duplicate("A", 1_024 * 1_024) end)
-      ...> ZipGz.stream([%{path: "rel/path/file.txt", source: [text]}], chunk_size: 10)
+      iex> text = Enum.map_join(1..10, "", fn _ -> String.duplicate("A", 1_024 * 1_024) end)
+      ...> entries = [%{path: "rel/path/file.txt", source: [text]}]
+      ...> ZipGz.stream(entries, chunk_size: 10, gzip: true)
 
   ### Streaming data from an enumerable source
 
-      iex> stream = Stream.map(1..100, fn _ -> 1_024 * 1_024 |> :crypto.strong_rand_bytes() |> Base.encode64() end)
-      ...> ZipGz.stream([%{path: "rel/path/file.txt", source: stream}])
+      iex> stream = Stream.map(1..10, fn _ -> :crypto.strong_rand_bytes(1_024 * 1_024) end)
+      ...> entries = [%{path: "rel/path/file.txt", source: stream}]
+      ...> ZipGz.stream(entries)
+
   """
 
   @best_compression :best_compression
-
-  # 32 MB - 549,453,824 bytes
-  @thirty_two_mib 32 * 1_024 * 1_024
+  @five_mib 5 * 1_024 * 1_024
 
   @doc """
   Creates a stream for compressing data into a ZIP archive, with optional GZIP compression.
@@ -38,8 +40,15 @@ defmodule ZipGz do
 
   ## Options
 
-    * `:chunk_size` - The maximum size of each chunk in bytes (default: 32 MiB).
+    * `:chunk_size` - Specifies the target size, in bytes, for each chunk of data in
+      the stream (default: 5 MiB). The stream will attempt to produce chunks of at
+      least this size. However, if the input data is smaller than the specified chunk
+      size, the stream will emit smaller chunks to ensure all data is processed. This
+      behavior ensures that no data is delayed or lost, even when the input size is
+      less than the target chunk size.
+
     * `:gzip` - Enables GZIP compression. Accepts:
+
       - `true` (default): Applies GZIP with default settings.
       - `false`: Disables GZIP compression.
       - A keyword list: Customizes GZIP options (e.g., `:level` for compression level).
@@ -47,25 +56,36 @@ defmodule ZipGz do
   ## Examples
 
       # Compress a single file with a chunk size of 10 bytes
-      iex> ZipGz.stream([%{path: "rel/path/file.txt", source: ["content"]}], chunk_size: 10)
+      iex> entries = [%{path: "rel/path/file.txt", source: ["content"]}]
+      ...> stream = ZipGz.stream(entries, chunk_size: 10)
+      ...> Enum.to_list(stream)
 
       # Compress a large string with GZIP and a chunk size of 10 bytes
       iex> text = Enum.map_join(1..1_024, "", fn _ -> String.duplicate("A", 1_024 * 1_024) end)
-      ...> ZipGz.stream([%{path: "rel/path/file.txt", source: [text]}], chunk_size: 10)
+      ...> entries = [%{path: "rel/path/file.txt", source: [text]}]
+      ...> stream = ZipGz.stream(entries, chunk_size: 10, gzip: true)
+      ...> Enum.to_list(stream)
 
       # Stream random data and compress it into a ZIP archive
-      iex> stream = Stream.map(1..100, fn _ -> 1_024 * 1_024 |> :crypto.strong_rand_bytes() |> Base.encode64() end)
-      ...> ZipGz.stream([%{path: "rel/path/file.txt", source: stream}])
+      iex> stream = Stream.map(1..10, fn _ -> :crypto.strong_rand_bytes(1_024 * 1_024) end)
+      ...> entries = [%{path: "rel/path/file.txt", source: stream}]
+      ...> stream = ZipGz.stream(entries)
+      ...> Enum.to_list(stream)
   """
   @spec stream(entries :: list(), opts :: keyword()) :: any()
   def stream(entries, opts \\ []) do
-    max = Keyword.get(opts, :chunk_size, @thirty_two_mib)
-
     entries
     |> normalize_entries()
     |> Zstream.zip()
     |> maybe_gzip_stream(opts)
-    |> ZipGz.Util.content_byte_stream(max)
+    |> maybe_chunk_stream(opts)
+  end
+
+  defp maybe_chunk_stream(stream, opts) do
+    case Keyword.get(opts, :chunk_size, @five_mib) do
+      :infinity -> stream
+      chunk_size -> ElixirUtils.Binary.chunk_stream(stream, chunk_size)
+    end
   end
 
   defp maybe_gzip_stream(stream, opts) do
@@ -79,7 +99,7 @@ defmodule ZipGz do
 
   defp normalize_entries(list) when is_list(list) do
     if Keyword.keyword?(list) do
-      [list |> Map.new() |> to_entry()]
+      [to_entry(list)]
     else
       Enum.map(list, &to_entry/1)
     end
@@ -93,12 +113,14 @@ defmodule ZipGz do
     Stream.map(enum, &to_entry/1)
   end
 
-  defp to_entry(params) do
-    opts =
-      params
-      |> Map.get(:options, [])
-      |> Keyword.put_new(:coder, Zstream.Coder.Stored)
+  defp to_entry(opts) when is_map(opts) do
+    opts |> Map.to_list() |> to_entry()
+  end
 
-    Zstream.entry(params.path, params.source, opts)
+  defp to_entry(opts) do
+    {path, o1} = Keyword.pop!(opts, :path)
+    {source, o2} = Keyword.pop!(o1, :source)
+    entry_opts = Keyword.put_new(o2, :coder, Zstream.Coder.Deflate)
+    Zstream.entry(path, source, entry_opts)
   end
 end
